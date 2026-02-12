b53b3567da895efaa59311b266dec8af4173a612
hhg <rdyhong@gmail.com>
2026-02-13 00:18:14 +0900

    * 폰트 추가, 하단 보드 로직 추가

--- UnityProject/Assets/Project/Scripts/InGameMainUI.cs
+++ UnityProject/Assets/Project/Scripts/InGameMainUI.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class InGameMainUI : MonoBehaviour
 {
+    public Canvas Canvas { get; private set; }
+
     public InGameUI InGameUI => _inGameUI;
     [SerializeField] private InGameUI _inGameUI;
 
     public void Initialize()
     {
+        Canvas = GetComponent<Canvas>();
         InGameUI.Initialize();
     }
 }
--- UnityProject/Assets/Project/Scripts/IngameBoardSlot.cs
+++ UnityProject/Assets/Project/Scripts/IngameBoardSlot.cs
@@ -2,46 +2,63 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using UnityEngine.Rendering;
+using TMPro;
+
+public class SlotData
+{
+    public int Grade { get; private set; } = 0;
+
+
+}
 
 public class IngameBoardSlot : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    [SerializeField] private float _grabScale = 1.2f;
-    [SerializeField] private float _scaleDuration = 0.1f;
-    [SerializeField] private float _returnDuration = 0.2f;
+    [SerializeField] private TextMeshProUGUI _txtGrade;
+
+    public SlotData SlotData { get; private set; } = null;
+
 
     public IngameBoardSlotCell SlotCell { get; private set; }
     public RectTransform Rt { get; private set; }
 
     public Vector2Int Coord { get; private set; }
 
-    private Canvas _canvas;
+    //private Canvas _canvas;
+
+    private float _grabScale = 1.2f;
+    private float _scaleDuration = 0.1f;
+    private float _returnDuration = 0.2f;
 
     public void Initialize(Vector2Int coord)
     {
         Rt = GetComponent<RectTransform>();
-
         Coord = coord;
-        
-        CreateSlotCell();
+        SlotData = new SlotData();
+        _txtGrade.text = string.Empty;
+
+        CreateSlotCell(new SlotData());
     }
 
-    public void CreateSlotCell()
+    public void CreateSlotCell(SlotData data)
     {
+        SlotData = data;
+
         IngameBoardSlotCell cell = ObjectPoolMgr.Inst.Spawn<IngameBoardSlotCell>("Project/Prefabs/UI/IngameBoardSlotCell");
         cell.Initialize();
         cell.Rt.SetParent(InGameMain.Inst.MainUI.InGameUI.BoardUI.RtCellsParent, false);
         cell.Rt.localScale = Vector3.one;
         cell.Rt.anchoredPosition3D = Rt.anchoredPosition3D;
+
         SlotCell = cell;
+
+        _txtGrade.text = SlotData.Grade.ToString();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         if (SlotCell.Rt == null) return;
 
-        // 캔버스 캐싱 (스크린→로컬 변환용)
-        if (_canvas == null)
-            _canvas = GetComponentInParent<Canvas>();
+        _txtGrade.text = string.Empty;
 
         SlotCell.Rt.DOKill();
         SlotCell.Rt.DOScale(_grabScale, _scaleDuration).SetEase(Ease.OutBack);
@@ -50,12 +67,12 @@ public class IngameBoardSlot : MonoBehaviour, IPointerDownHandler, IDragHandler,
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (SlotCell.Rt == null || _canvas == null) return;
+        if (SlotData == null) return;
 
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             SlotCell.Rt.parent as RectTransform,
             eventData.position,
-            _canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : _canvas.worldCamera,
+            InGameMain.Inst.MainUI.Canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : InGameMain.Inst.MainUI.Canvas.worldCamera,
             out Vector2 localPoint);
 
         SlotCell.Rt.localPosition = localPoint;
@@ -64,6 +81,8 @@ public class IngameBoardSlot : MonoBehaviour, IPointerDownHandler, IDragHandler,
     public void OnPointerUp(PointerEventData eventData)
     {
         if (SlotCell.Rt == null) return;
+
+        _txtGrade.text = SlotData.Grade.ToString();
 
         SlotCell.Rt.DOKill();
         SlotCell.Rt.DOLocalMove(Rt.anchoredPosition3D, _returnDuration).SetEase(Ease.OutBack);
